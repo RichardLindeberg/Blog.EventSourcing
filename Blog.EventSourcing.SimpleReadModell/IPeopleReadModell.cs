b4bb7cdf46be7e7516b317b3f76681be03a5dfd1
rille112@hotmail.com <rille112@hotmail.com>
2016-09-25 21:36:55 +0200

    no message

---
+++ Blog.EventSourcing.SimpleReadModell/IPeopleReadModell.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using Blog.EventSourcing.Events.Person;
+
+namespace Blog.EventSourcing.SimpleReadModell
+{
+    public interface IPeopleReadModell
+    {
+        IEnumerable<NameAndEmail> NameAndEmails { get; }
+
+        void Handle(IPersonEvent evt);
+    }
+}
